c49571b774ff77bfe963b6074c6a645696bd3cfa
EmiriNakamori <emiri060414@gmail.com>
2024-12-14 13:11:59 +0900

    Initial Commit

---
+++ ImageEnglish/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ImageEnglish
+//
+//  Created by えみり on 2024/12/14.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
