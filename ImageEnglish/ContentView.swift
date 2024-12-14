ce64a9fa87e2a9c4bb99d8d075320025f0a87a8e
EmiriNakamori <emiri060414@gmail.com>
2024-12-14 18:28:08 +0900

    fix: flipしながら保存した画像を表示するviewを表示

--- ImageEnglish/ContentView.swift
+++ ImageEnglish/ContentView.swift
@@ -16,54 +16,136 @@ struct ContentView: View {
     @State private var showSheet = false
     @State private var createdImageURL: URL? = nil
     @Environment(\.supportsImagePlayground) private var supportsImagePlayground
+    @State var uiImage: UIImage? = nil
+    @State private var isShowAlert = false
+    @State private var isFlipped = false
+    
+    init() {
+      UITextField.appearance().clearButtonMode = .whileEditing
+    }
 
     var body: some View {
         NavigationSplitView {
-            TextField("enterItem", text: $newText)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                .background(Color(.secondarySystemBackground))
-                .cornerRadius(8)
-                .padding()
-            
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        if let url = item.imageUrl, let uiImage = UIImage(contentsOfFile: url.path) {
-                            Image(uiImage: uiImage)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        } else {
-                            Image(systemName: "photo")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 300, height: 300)
-                                .foregroundColor(.gray)
+            ZStack {
+                Rectangle()
+                    .rotation3DEffect(
+                        .degrees(isFlipped ? 180 : 0),
+                        axis: (x: 0, y: 1, z: 0)
+                    )
+                    
+                VStack {
+                    if isFlipped {
+                        VStack {
+                            Text("CollectionViewで画像表示する")
+                                .font(.title)
+                                .foregroundColor(.green)
+                                .padding()
                         }
-                    } label: {
-                        Text("\(item.text)")
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                
-                ToolbarItem {
-                    Button {
-                        showSheet = true
-                    } label: {
-                        Label("Sheet Image", systemImage: "apple.image.playground")
-                    }
-                    .imagePlaygroundSheet(
-                        isPresented: $showSheet,
-                        concept: newText) { url in
-                            createdImageURL = url
-                            addItem()
+                        .toolbar {
+                            ToolbarItem {
+                                Button {
+                                    withAnimation {
+                                        isFlipped.toggle()
+                                    }
+                                } label: {
+                                    Image(systemName: "arrow.trianglehead.2.clockwise.rotate.90")
+                                        .foregroundStyle(.orange)
+                                }
+                            }
                         }
+                        
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.black)
+                    } else {
+                        VStack {
+                            TextField("Enter Keywords", text: $newText)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+                                .background(Color(.secondarySystemBackground))
+                                .cornerRadius(8)
+                                .padding()
+                            
+                            List {
+                                ForEach(items) { item in
+                                    NavigationLink {
+                                        VStack {
+                                            if let url = item.imageUrl, let uiImage = UIImage(contentsOfFile: url.path) {
+                                                Image(uiImage: uiImage)
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                                    .onAppear {
+                                                        self.uiImage = uiImage
+                                                    }
+                                            } else {
+                                                Image(systemName: "photo")
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(width: 300, height: 300)
+                                                    .foregroundColor(.gray)
+                                            }
+                                        }
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                        .background(Color.black)
+                                        .toolbar {
+                                            ToolbarItem {
+                                                Button {
+                                                    saveImageToPhotos()
+                                                } label: {
+                                                    Image(systemName: "square.and.arrow.down")
+                                                        .foregroundColor(.white)
+                                                }
+                                                .alert(isPresented: $isShowAlert) {
+                                                    Alert(title: Text("Image saved successfully!"))
+                                                }
+                                            }
+                                        }
+                                        
+                                    } label: {
+                                        Text("\(item.text)")
+                                    }
+                                    
+                                }
+                                .onDelete(perform: deleteItems)
+                            }
+                            .scrollContentBackground(.hidden)
+                            .background(Color.black)
+                            .toolbar {
+                                ToolbarItem(placement: .navigationBarTrailing) {
+                                    EditButton()
+                                        .foregroundStyle(.white)
+                                }
+                                
+                                ToolbarItem {
+                                    Button {
+                                        showSheet = true
+                                    } label: {
+                                        Image(systemName: "apple.image.playground")
+                                            .foregroundStyle(.linearGradient(Gradient(colors: [.purple, .cyan, .orange]), startPoint: .top, endPoint: .bottom))
+                                    }
+                                    .imagePlaygroundSheet(
+                                        isPresented: $showSheet,
+                                        concept: newText) { url in
+                                            createdImageURL = url
+                                            addItem()
+                                        }
+                                }
+                                
+                                ToolbarItem {
+                                    Button {
+                                        withAnimation {
+                                            isFlipped.toggle()
+                                        }
+                                    } label: {
+                                        Image(systemName: "arrow.trianglehead.2.clockwise.rotate.90")
+                                            .foregroundStyle(.orange)
+                                    }
+                                }
+                                
+                            }
+                        }
+                        .background(.black)
+                    }
                 }
             }
         } detail: {
@@ -86,6 +168,13 @@ struct ContentView: View {
             }
             try? modelContext.save()
         }
+    }
+    
+    private func saveImageToPhotos() {
+        guard let uiImage = uiImage else { return }
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: uiImage)
+        isShowAlert = true
     }
 }
 
---
+++ ImageEnglish/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  ImageEnglish
+//
+//  Created by えみり on 2024/12/14.
+//
+
+import SwiftUI
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save Image finished!")
+    }
+}
